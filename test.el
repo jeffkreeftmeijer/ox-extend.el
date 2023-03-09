7bb8640f4542a76ef1957bd0353d7d1b541507e7
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-03-09 10:54:17 +0100

    Add "hello world" example and ERT test

---
+++ test.el
@@ -0,0 +1,25 @@
+(require 'ert)
+(add-to-list 'load-path ".")
+(require 'ox-extend)
+
+(add-to-list
+ 'ox-extend-extensions-alist '('hello-world :add (lambda () (message "hello, world!"))
+                                            :remove (lambda () (message "goodbye, world!"))))
+
+(ert-deftest hello-world-test ()
+  (ox-extend-add)
+
+  (org-publish-file "ox-extend.org"
+                    '("ox-extend-example"
+                      :base-directory "."
+                      :publishing-directory "dist"
+                      :extensions ('hello-world)))
+
+  (let ((output (with-current-buffer "*Messages*" (buffer-string))))
+    (let ((lines (split-string output "\n")))
+
+      (should (string-match-p "hello, world!" output))
+      (should (string-match-p "ox-extend.org" output))
+      (should (string-match-p "goodbye, world!" output))))
+
+  (delete-directory "dist" t))
